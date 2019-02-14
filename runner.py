3fb2f5b4c7b9ef8211473744a0522f9b2fd268f3
andrew-dewitt <adwtt90@gmail.com>
2019-02-14 16:32:21 -0500

    refactored to use a new logic that passes a TestResult object for better extensability

--- runner.py
+++ runner.py
@@ -4,83 +4,151 @@ COLOR_GREEN = "\033[32m"
 COLOR_RED = "\033[31m"
 COLOR_RESET = "\033[0m"
 
-def expect(actual):
-    return Actual(actual)
+def describe(description):
+    return Describe(description)
 
-def raise_error(exception_class):
-    return Expectations.Error(exception_class)
+# def expect(test):
+#     return Actual(test)
+# 
+# def eq(expected):
+#     return Expectations.Equal(expected)
 
-def eq(expected):
-    return Expectations.Equal(expected)
+# def raise_error(exception_class):
+#     return Expectations.Error(exception_class)
 
 class Describe:
     def __init__(self, description, parent=None):
         self.description = description
-        self.tests = {}
+        self.tests = []
         self.tab = '\t'
 
-        if not parent is None:
-            self.parent = parent
-            self.tab = self.parent.tab + self.tab
+    def it(self, description, code):
+        test_obj = Test(description, code)
+        self.tests.append(test_obj)
 
-    def it(self, name, block):
-        self.tests[name] = block
+        return test_obj
 
     def run(self):
         print(self.description)
-        self.test()
 
         for test in self.tests:
-            print(f'  - {test}', end='')
-            try:
-                self.tests[test]()
-                print(f': {COLOR_GREEN}ok{COLOR_RESET}')
-            except Exception as e:
-                print(f': {COLOR_RED}fail{COLOR_RESET}')
+            print(f'  - {test.name}', end='')
 
-                tb = traceback.format_exc().splitlines()
+            if test.success == True:
+                print(f': {COLOR_GREEN}ok{COLOR_RESET}')
+            else:
+                print(f': {COLOR_RED}fail{COLOR_RESET}')
 
                 print(f'{self.tab}{COLOR_RED}* STACK TRACE{COLOR_RESET}')
 
-                for line in tb[:-1]:
+                for line in test.tb[:-1]:
                     print(f'{self.tab}{COLOR_RED}|{COLOR_RESET} {line}')
 
-                print(f'{self.tab}{COLOR_RED}* {tb[-1]}{COLOR_RESET}')
+                print(f'{self.tab}{COLOR_RED}* {test.tb[-1]}{COLOR_RESET}')
 
-class Actual:
-    def __init__(self, actual):
-        self.actual = actual
+class Test:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+        self.should = self.init_should()
 
-    def to(self, expectation):
-        return expectation.run(self.actual) 
+    def init_should(self):
+        return self.Should(self.code, self)
 
-class Expectations:
-    def __init__(self):
-        self.Equal = Equal
+    class Should:
+        def __init__(self, code, test_called):
+            self.code = code
+            self.test_called = test_called
 
-    def err_msg(actual, expected):
-        return f"Expected {expected}, but got {actual}"
+        def _code_result(self):
+            return self.code() if callable(self.code) else self.code
 
-    class Equal:
-        def __init__(self, expected):
-            self.expected = expected
-
-        def run(self, actual):
+        def eq(self, expected):
             try:
-                actual_res = actual() if callable(actual) else actual 
-            except Exception as e:
-                actual_res = type(e)
+                code_result = self._code_result()
 
-            assert actual_res == self.expected, Expectations.err_msg(actual_res, self.expected)
+                if not code_result == expected:
+                    raise AssertionError(f'expected {expected}, but got {code_result}')
 
-    class Error:
-        def __init__(self, exception_class):
-            self.exception_class = exception_class
+                self.test_called.success = True
 
-        def run(self, actual):
+            except Exception as err:
+                self.test_called.success = False
+                self.test_called.err = err
+                self.test_called.tb = traceback.format_exc().splitlines()
+
+            return self.test_called
+
+        def raise_error(self, expected_err):
             try:
-                actual_res = actual() if callable(actual) else actual 
-            except Exception as e:
-                actual_res = type(e)
+                code_result = self._code_result()
 
-            assert actual_res == self.exception_class, Expectations.err_msg(actual_res, self.exception_class)
+                self.test_called.success = False
+                self.test_called.err = 'No error was raised'
+                self.test_called.tb = ['No error was raised']
+
+            except Exception as err:
+                if not type(err) == expected_err:
+                    raise AssertionError(f'expected {expected_err}, but got {err}')
+
+                self.test_called.success = True
+
+            except AssertionError as err:
+                self.test_called.success = False
+                self.test_called.err = err
+                self.test_called.tb = traceback.format_exc().splitlines()
+
+            return self.test_called
+
+# class Actual:
+#     def __init__(self, actual):
+#         self.actual = actual
+# 
+#     def to(self, expectation):
+#         return expectation.run(self.actual) 
+# 
+# class Expectations:
+#     def __init__(self):
+#         self.Equal = Equal
+# 
+#     def err_msg(actual, expected):
+#         return f"Expected {expected}, but got {actual}"
+# 
+#     class Equal:
+#         def __init__(self, expected):
+#             self.expected = expected
+# 
+#         def run(self, actual):
+#             try:
+#                 actual_res = actual() if callable(actual) else actual 
+#             except Exception as e:
+#                 actual_res = type(e)
+# 
+#             try:
+#                 assert actual_res == self.expected, Expectations.err_msg(
+#                     actual_res,
+#                     self.expected)
+#             except AssertionError as e:
+#                 return Expectations.Result(False, e)
+# 
+#             return Expectations.Result(True)
+# 
+#     class Error:
+#         def __init__(self, exception_class):
+#             self.exception_class = exception_class
+# 
+#         def run(self, actual):
+#             try:
+#                 actual_res = actual() if callable(actual) else actual 
+#             except Exception as e:
+#                 actual_res = type(e)
+# 
+#             assert actual_res == self.exception_class, Expectations.err_msg(
+#                 actual_res,
+#                 self.exception_class)
+# 
+#     class Result:
+#         def __init__(self, success, name, err=None):
+#             self.success = success
+#             self.name = name
+#             self.err = err
--- runner_spec.py
+++ runner_spec.py
@@ -1,111 +1,109 @@
 from runner import *
 
+### EXPECTATIONS ###
 # Describe a group of tests by defining a class that inherits from 
 # Runner's Describe class
-class Expectations(Describe):
-    # then define a set of tests in that class' `test` method
-    def test(self):
-        # tests can be set up to expect a return value
-        def can_expect_values():
-            # which can be tested for by passing an expression to test to Runner's 
-            # `expect()` function & asserting a value it should equal using `to(eq(...))`
-            # read the following line like 'expect one plus one to equal two'
-            expect(1 + 1).to(eq(2))
-        # then use the `it()` method of your created child of Describe to 
-        # name & store the test
-        self.it('can expect values', can_expect_values)
+# class Expectations(Describe):
+expectations = describe('set expectations')
 
-        # tests can also be set up to expect a specific error class
-        def can_expect_exceptions():
-            # complicated expressions (or those that you want to defer the evaluation of
-            # until the test is run) can be defined as a function within the test, 
-            def divide_by_zero():
-                return 1/0
+# tests can be set up to expect a return value
+expectations.it('can expect values',
+    # which can be tested for by passing a function as the second argument to 
+    # `it()`; functions are required to deferr the execution of any passed expression
+    # read the following line as one plus one should equal two
+    1 + 1).should.eq(2)
 
-            # then passed, uncalled, to `expect()`
-            # to test for an expected error, use Runner's `raise_error()` method, 
-            # giving it the expected error class
-            # this line can be read like 'expect *expression* to raise a ZeroDivisionError'
-            expect(divide_by_zero).to(raise_error(ZeroDivisionError))
-        # all tests are named & stored using the same pattern
-        self.it('can expect exceptions', can_expect_exceptions)
+# tests can also be set up to expect a specific error class
+expectations.it('can expect exceptions',
+    lambda: 1/0).should.raise_error(ZeroDivisionError)
 
-        # you can also test that a given test will always fail as expected
-        # def correct_failure_type():
-        #     expect(expect(1).to(eq(2))).to(raise_error(AssertionError))
+# A group of tests is ran by calling it's `run()` method
+expectations.run()
 
-# A group of tests is ran by initializing the defined class with a description, 
-# then calling the `run()` method it inherits from `Describe`
-Expectations('set expectations & test versus actual expression results').run()
-
+### COMMON###
 # You can also define common variables for a group of tests
-class Let(Describe):
-    # this can be done any of a few of ways:
+common = Describe('set common state')
 
-    # first, name a class variable & assign it a value
-    five = 5
+# this is done by creating new attributes on the test group
+common.five = 5
+# you can assign simple values (or values from expressions) as above,
+# or you can define a function & assign it as a new method
+def five_mthd(): return 5
+common.five_mthd = five_mthd
 
-    # or second, define a class @property
-    @property
-    def five_prop(self): return 5
+common.it('can use common attributes',
+    common.five).should.eq(5)
 
-    # or third, define an instance variable in `__init__`
-    def __init__(self, description):
-        # this requires first calling Describe's init & passing `description`
-        # otherwise the other methods of Describe won't be initialized in the right order
-        super().__init__(description)
+common.it('can use common methods',
+    common.five_mthd).should.eq(5)
 
-        # then you can name your instance variable & assign it a value
-        self.five_inst = 5
+common.run()
 
-    # then define your tests that may use these variables just as before
-    def test(self):
-        def available_inside_tests():
-            # and refer to the common variable using `self.variable_name`
-            expect(self.five).to(eq(5))
-            expect(self.five_prop).to(eq(5))
-            expect(self.five_inst).to(eq(5))
-        self.it('available inside the tests', available_inside_tests)
+### FAILURES ###
+# you can also test that a given test will always fail as expected
+failures = describe('communicate failures')
 
-Let('create local state for testing purposes').run()
+# to do this, first create a failing test
+failures.fail = Describe('failure').it('should fail', 1).should.eq(2)
 
-# You can also have one group of tests inherit class & instance variables from
-# another group of tests by passing the intended parent class 
-# instead of Runner's `Describe` class
-class Inherit(Let):
-    def test(self):
-        def see_inherited_variables():
-            # then you can refer to it as if it were a variable of this class
-            # instance:
-            expect(self.five).to(eq(5))
-            # class:
-            expect(self.five_prop).to(eq(5))
-            expect(self.five_inst).to(eq(5))
-        self.it('see inherited variables', see_inherited_variables)
+# create a new function to deferr re-raising the error
+def failed():
+    # & grab the error off the test result object & re-raise it
+    raise failures.fail.err
 
-        # you can even modify the inherited instance & class variables without
-        # modifying it in the parent test group class
-        def modify_inherited_variables():
-            self.five_inst = 6
-            expect(self.five_inst).to(eq(6))
-        self.it('modify inherited variables', modify_inherited_variables)
+# then assign that function to a method on common state for the test group
+failures.failed = failed
+# you can grab just the error message from the args attribute of the returned error
+failures.failed_msg = failures.fail.err.args[0]
 
-        # unmodified variables from the parent test group can be accessed directly
-        def access_original_variable():
-            # do this by initializing an instance of the parent & requesting 
-            # it's instance variable directly with dot notation
-            expect(Let('let').five_inst).to(eq(5))
+# lastly, call it on the common method as the expected value with
+# AssertionError as the error that should be raised by the failing test
+failures.it('can show the expected error type',
+    failures.failed).should.raise_error(AssertionError)
 
-            # or by initializing it & assigning it to a variable, then
-            # using it as needed
-            let = Let('let')
-            six = let.five_inst + 1
-            expect(six).to(eq(6))
-        self.it('access original variable from parent', access_original_variable)
+failures.it('can show the expected error message',
+    failures.failed_msg).should.eq('expected 2, but got 1')
 
-        # but original values are not changed in the parent test group
-        def original_values_unchanged():
-            expect(Let('let').five_inst).to(eq(5))
-        self.it("won't change original values in parent", original_values_unchanged)
+failures.run()
 
-Inherit('inherit state from a parent test group').run()
+# 
+# # You can also have one group of tests inherit class & instance variables from
+# # another group of tests by passing the intended parent class 
+# # instead of Runner's `Describe` class
+# class Inherit(Let):
+#     def test(self):
+#         def see_inherited_variables():
+#             # then you can refer to it as if it were a variable of this class
+#             # instance:
+#             expect(self.five).to(eq(5))
+#             # class:
+#             expect(self.five_prop).to(eq(5))
+#             expect(self.five_inst).to(eq(5))
+#         self.it('see inherited variables', see_inherited_variables)
+# 
+#         # you can even modify the inherited instance & class variables without
+#         # modifying it in the parent test group class
+#         def modify_inherited_variables():
+#             self.five_inst = 6
+#             expect(self.five_inst).to(eq(6))
+#         self.it('modify inherited variables', modify_inherited_variables)
+# 
+#         # unmodified variables from the parent test group can be accessed directly
+#         def access_original_variable():
+#             # do this by initializing an instance of the parent & requesting 
+#             # it's instance variable directly with dot notation
+#             expect(Let('let').five_inst).to(eq(5))
+# 
+#             # or by initializing it & assigning it to a variable, then
+#             # using it as needed
+#             let = Let('let')
+#             six = let.five_inst + 1
+#             expect(six).to(eq(6))
+#         self.it('access original variable from parent', access_original_variable)
+# 
+#         # but original values are not changed in the parent test group
+#         def original_values_unchanged():
+#             expect(Let('let').five_inst).to(eq(5))
+#         self.it("won't change original values in parent", original_values_unchanged)
+# 
+# Inherit('inherit state from a parent test group').run()
