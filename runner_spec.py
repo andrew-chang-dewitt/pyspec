6dc0717a8f1ada2edb214bf013879553a5e794c3
andrew-dewitt <adwtt90@gmail.com>
2019-02-15 16:13:10 -0500

    adding more inline comments as documentation

--- runner_spec.py
+++ runner_spec.py
@@ -67,36 +67,50 @@ failures.it('can show the expected error message',
 
 failures.run()
 
-####
-# FIXME: below needs changed into new syntax
-#      : features may need added
-
 # You can also have one group of tests inherit state from another
+# for example, you may have a standard test group
 outer = describe('outer')
 
+# with some state containing methods & attributes
 def method(arg):
     return f'cool method, {arg}'
 
 outer.attr = 'attribute'
 outer.mthd = method
 
+# then you want to run another set of tests that may use some of those
+# same methods & attributes, but exist as a separate test group
+# to designate this new group as 'nested within' or inheriting from another, 
+# just pass the first group as a second argument to `describe()`
 inner = describe('inner', outer)
 
+# this inner, nested test will be able to referr to the outer group's 
+# attributes & methods as if it were its own without having to directly
+# refer to the outer test group, seen below by using `inner.attr`, although
+# an `attr` attribute was never explicitly defined on inner
 inner.it('can see outer attributes',
     inner.attr).should.eq('attribute')
 
 inner.it('can use outer methods',
     lambda: inner.mthd('brah')).should.eq('cool method, brah')
 
+# the nested test can then change the values of any inherited attributes or
+# methods & continue to use them as desired
 inner.attr = 'inner attribute'
 
 inner.it('can redefine/reassign outer methods/attributes',
     inner.attr).should.eq('inner attribute')
 
+# but the outer, enclosing group will still retain its orinal value for the 
+# changed attribute
 outer.it('but outer methods/attributes will remain unchanged',
     outer.attr).should.eq('attribute')
 
+# then, you can always refer to an attribute of another test group by explicitly
+# specifying it, seen here by using `inner.attr` in a test run by `outer`
 outer.it('outer groups can also access the attributes of an inner group',
     inner.attr).should.eq('inner attribute')
 
+# lastly, inner test groups do not need to be explicitly called, as they will
+# instead be called by their enclosing test group at run time
 outer.run()
