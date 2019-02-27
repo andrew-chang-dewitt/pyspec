90b0ddee5c0e1e15e8a89701ba844e9243e7064f
andrew-dewitt <adwtt90@gmail.com>
2019-02-27 10:25:38 -0500

    click help text complete, using docstrings

--- pyspec/cli/entry.py
+++ pyspec/cli/entry.py
@@ -9,14 +9,25 @@ from pyspec.api import describe
 
 @click.group()
 def main():
+    """
+    CLI companion tool for the Pyspec testing library. 
+    """
     pass
 
-@main.command()
+@main.command('all')
 @click.argument('path')
-def all(path):
+def all_tests(path):
+    """
+    Runs all tests in a given directory. PATH must be relative to the current $PWD.
+    This command will only find files in the given directory that end in `_spec.py`.
+    """
     run_tests.all_tests(path)
 
 @main.command()
 @click.argument('module')
 def one(module):
+    """
+    Runs the specific test file given as a module name. MODULE must be just the file
+    name, without any file type extensions.
+    """
     run_tests.one_file(module)
