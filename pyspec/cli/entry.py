2208d9f389dca4fda841897a858422c0066990d6
andrew-dewitt <adwtt90@gmail.com>
2019-02-27 10:19:59 -0500

    refactored one & all into separate commands in a click.group, adding help text next

--- pyspec/cli/entry.py
+++ pyspec/cli/entry.py
@@ -7,19 +7,16 @@ import click
 from pyspec.cli import run_tests
 from pyspec.api import describe
 
-@click.command()
-@click.argument('method')
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.argument('path')
+def all(path):
+    run_tests.all_tests(path)
+
+@main.command()
 @click.argument('module')
-def main(method, module):
-    def all_tests(path):
-        run_tests.all_tests(path)
-
-    def one_file(path):
-        run_tests.one_file(path)
-
-    options = {
-        'all': all_tests,
-        'one': one_file
-    }
-
-    options[method](module)
+def one(module):
+    run_tests.one_file(module)
