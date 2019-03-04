ffdddeffe0b75e63842b66c270d767f5e95d4af0
andrew-dewitt <adwtt90@gmail.com>
2019-03-04 16:44:44 -0500

    end result test written, expected result will need modified using regex or something else?

---
+++ tests/stats_spec.py
@@ -0,0 +1,23 @@
+"""tests for SpecStruct metastructure"""
+
+import pyspec
+
+RUNNER = pyspec.spec_struct()
+
+STATS = pyspec.describe('fetch stats info', RUNNER)
+
+def test_stats():
+    """
+    Test run to get stats
+    """
+    STATS.test_group = pyspec.describe('test stats')
+    STATS.test_group.it('can do stuff', 1).should.eq(1)
+    STATS.test_results = STATS.test_group.run()
+    stats_line = STATS.test_results[-1]
+
+    return stats_line
+
+STATS.it(
+    'prints stats on the last line of a test',
+    test_stats
+    ).should.eq('Total time:')
