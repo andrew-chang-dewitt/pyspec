9a0f6373a6203eb7bdd0109ba2a11cf6e1d134d2
Andrew DeWitt <adwtt90@gmail.com>
2019-03-15 15:01:26 -0400

    resolved FileNotFoundError

--- tests/cli_spec.py
+++ tests/cli_spec.py
@@ -15,23 +15,27 @@ TEMP = PATH + '/temp'
 os.mkdir(TEMP)
 with open(TEMP + '/temp_spec.py', 'w') as temp_spec:
     temp_spec.write('from pyspec import describe, spec_struct')
+    temp_spec.write('\n')
     temp_spec.write('RUNNER = spec_struct()')
+    temp_spec.write('\n')
     temp_spec.write('TEST = describe("this is a test")')
+    temp_spec.write('\n')
     temp_spec.write('TEST.it("can pass", 1).should.eq(1)')
+    temp_spec.write('\n')
     temp_spec.write('TEST.it("can fail", 1).should.eq(2)')
+    temp_spec.write('\n')
 
 for item in os.scandir(TEMP):
     print(item)
 
 COMMANDS.it(
     'can run all tests in a given directory',
-    lambda: cli.run_tests.all_tests('tests/temp')
+    lambda: cli.run_tests.all_tests('temp')
 ).should.eq(True)
 
 COMMANDS.it(
     'can run just the tests for one file',
-    lambda: cli.run_tests.one_file('tests/temp/temp_spec')
+    lambda: cli.run_tests.one_file('temp/temp_spec')
 ).should.eq(True)
 
-# this is being called before line 33 is executed, somehow this needs to be fixed
 shutil.rmtree(TEMP)
