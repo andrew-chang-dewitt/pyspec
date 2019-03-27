66bb25ffe25ff811e0f607b5be9b7713b12a359a
Andrew DeWitt <adwtt90@gmail.com>
2019-03-27 16:50:32 -0400

    adding explore method to cli, needs final review before approving for merge

--- tests/cli_spec.py
+++ tests/cli_spec.py
@@ -18,3 +18,8 @@ COMMANDS.it(
     'can run just the tests for one file',
     COMMANDS.cli_run.one_file('tests/test_examples/temp_spec', True)
 ).should.be_a(Runner)
+
+COMMANDS.it(
+    'can request a list of all test groups in a test directory',
+    lambda: COMMANDS.cli_run.explore('tests/test_examples', True)[0].description
+).should.eq('this is a test')
