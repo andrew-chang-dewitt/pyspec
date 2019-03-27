da5b9082ffbdd8caabe6331cd164dc9e07cf973a
Andrew DeWitt <adwtt90@gmail.com>
2019-03-27 15:16:41 -0400

    all tests passing, ready to merge

--- tests/cli_spec.py
+++ tests/cli_spec.py
@@ -8,22 +8,13 @@ from pub_sub import stable
 
 COMMANDS = describe('the cli has commands for running *_spec files')
 
-def test_all():
-    cli_run = cli.run_tests.RunTests(stable.event('CLI test'))
-
-    return cli_run.all_tests('tests/test_examples', True)
+COMMANDS.cli_run = cli.run_tests.RunTests(stable.event('temp spec'))
 
 COMMANDS.it(
     'can run all tests in a given directory',
-    test_all
-).should.be_a(Runner)
-
-def test_one():
-    cli_run = cli.run_tests.RunTests(stable.event('CLI test no. 2'))
-
-    return cli_run.one_file('tests/test_examples/temp_spec', True)
+    lambda: COMMANDS.cli_run.all_tests('tests/test_examples', True)).should.be_a(Runner)
 
 COMMANDS.it(
     'can run just the tests for one file',
-    test_one
+    COMMANDS.cli_run.one_file('tests/test_examples/temp_spec', True)
 ).should.be_a(Runner)
