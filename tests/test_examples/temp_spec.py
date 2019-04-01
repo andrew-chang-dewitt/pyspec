ead5d6e7babd0bbf81af7b395858259def68c3b1
Andrew DeWitt <adwtt90@gmail.com>
2019-04-01 15:24:17 -0400

    cli tests & example test passing

--- tests/test_examples/temp_spec.py
+++ tests/test_examples/temp_spec.py
@@ -1,9 +1,11 @@
 import  pyspec
 from pub_sub import stable
 
+C = pyspec.Comparisons
+
 PUB_SUB = stable.event('temp spec')
 
 TEST = pyspec.describe('this is a test', None, PUB_SUB)
 
-TEST.it('can pass', 1).should.eq(1)
-TEST.it('can fail', 1).should.eq(2)
+TEST.it('can pass').expect(1).to(C.eq, 1)
+TEST.it('can fail').expect(1).to(C.eq, 2)
