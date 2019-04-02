8cb1df5dd7b65e849e4bb39f4c2883a0a7a6f36b
Andrew DeWitt <adwtt90@gmail.com>
2019-04-02 12:39:18 -0400

    adding py-pub-sub github link for setuptools installation

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=['Click'],
+    dependency_links=['https://github.com/andrew-dewitt/py-pub-sub.git'],
     entry_points='''
         [console_scripts]
         pyspec=pyspec.cli.entry:entry_point
