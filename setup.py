b32d93291543e4a8bd3fd50df54caba3f7e5ab19
Andrew DeWitt <adwtt90@gmail.com>
2019-04-02 14:35:15 -0400

    error fix

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     name='pyspec',
     version='0.1.0',
     packages=find_packages(),
-    dependency_links=['https://github.com/andrew-dewitt/py-pub-sub/tarball/master#egg=py-pub-sub'],
+    dependency_links=[
+        'https://github.com/andrew-dewitt/py-pub-sub/tarball/master#egg=py-pub-sub-0.0.1'
+    ],
     install_requires=[
-        'Click',
-        'py-pub-sub'
+        'Click==7.0',
+        'py-pub-sub>=0.0.1'
     ],
     entry_points='''
         [console_scripts]
