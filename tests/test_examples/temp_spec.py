9af28fb676aed6fa8c3f7a5c646e601d6244b4b2
Andrew DeWitt <adwtt90@gmail.com>
2019-04-03 09:03:36 -0400

    nesting of groups is functioning correctly

--- tests/test_examples/temp_spec.py
+++ tests/test_examples/temp_spec.py
@@ -7,7 +7,7 @@ C = pyspec.Comparisons
 
 PUB_SUB = stable.event('temp spec')
 
-TEST = pyspec.describe('this is a test', None, PUB_SUB)
+TEST = pyspec.describe('this is a test', PUB_SUB)
 
 TEST.it('can pass').expect(lambda: 1).to(C.eq, 1)
 TEST.it('can fail').expect(lambda: 1).to(C.eq, 2)
