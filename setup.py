95a1f6f98d37b06c3e7e080c317bbaab9b550986
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-02-14 16:07:52 -0500

    updating setup.py & removing old dependency links no longer required

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import find_packages
 setup(
     name='pyspec',
     version='1.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*", "example*"]),
+    include_package_data=True,
     install_requires=[
         'Click==7.0',
-        'pub-sub @ git+git://github.com/andrew-dewitt/py-pub-sub#egg=pub-sub'
+        'pub-sub>=0.0.2'
     ],
     entry_points='''
         [console_scripts]
